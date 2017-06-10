9e9b3fb96ad7583ebff064e114f7faa5399bb2c0
Cream94 <creamcra@gmail.com>
2017-06-10 17:24:12 +0700

    update

--- index.php
+++ index.php
@@ -5,8 +5,41 @@
 <?php include 'header.php' ?>
 </head>
 <body>
-    <div class="container">
-      
+  <?php include 'navbar.php' ?>
+    <div class="container-fluid">
+
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <form class="form-horizontal">
+      <div class="form-group">
+      <label for="username" class="col-sm-2 control-label">Username</label>
+      <div class="col-sm-10">
+      <input type="username" class="form-control" id="username" placeholder="Username">
+      </div>
+      </div>
+      <div class="form-group">
+      <label for="password" class="col-sm-2 control-label">Password</label>
+      <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" placeholder="Password">
+      </div>
+      </div>
+      <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+      </div>
+      </div>
+      <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Login</button>
+      </div>
+      </div>
+      </form>
+        </div>
+      </div>
     </div>
 </body>
 </html>
