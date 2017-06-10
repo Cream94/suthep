93fca2a2fc664c7fd4418c6a4819a28f3890d35c
apimun <apimun.ksk@gmail.com>
2017-06-10 14:45:46 +0700

    edit js

--- header.php
+++ header.php
@@ -4,5 +4,5 @@
 <link rel="stylesheet" href="dist/css/bootstrap.css">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="dist/js/jquery-3.2.1.min.js">
-<script src="dist/js/bootstrap.js">
+<script src="dist/js/jquery-3.2.1.min.js"></script>
+<script src="dist/js/bootstrap.js"></script>
